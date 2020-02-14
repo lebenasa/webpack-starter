cf6fc468f9c7c6a0929ea92d7171f68aa24f301f
Leben Asa <lebenasa@hotmail.com>
2020-02-14 10:14:56 +0700

    Basic webpack config with vanilla JS and Sass

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.sass';
+
+function component() {
+    const element = document.createElement("h1");
+    element.innerText = "Hello world";
+    return element;
+}
+
+document.body.appendChild(component());
