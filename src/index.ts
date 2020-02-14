322bf14f287d1d969142f27d2056275e3f781c10
Leben Asa <lebenasa@hotmail.com>
2020-02-14 11:52:21 +0700

    Add linter for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './index.html'
 import './style.sass'
 
-function component() {
+function component(): HTMLElement {
     const element = document.createElement('h1')
     element.innerHTML = 'Hello world!!'
     return element
