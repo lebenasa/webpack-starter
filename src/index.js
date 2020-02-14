d22ceb0462773bc14224143bcb992db3ed647c6d
Leben Asa <lebenasa@hotmail.com>
2020-02-14 10:35:25 +0700

    Add HTML loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './index.html';
 import './style.sass';
 
 function component() {
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,21 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.html$/i,
+                include: [
+                    path.resolve(__dirname, 'src/'),
+                ],
+                use: [
+                    'file-loader?name=[name].[ext]',
+                    'extract-loader',
+                    'html-loader',
+                ],
+            },
+            {
                 test: /\.s[ac]ss$/i,
+                include: [
+                    path.resolve(__dirname, 'src/'),
+                ],
                 use: [
                     // Creates `style` nodes from JS strings
                     'style-loader',
